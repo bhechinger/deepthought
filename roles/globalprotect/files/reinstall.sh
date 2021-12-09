6cbed182fbbc50a5224d52b74b167a83e21a6492
bhechinger <wonko@4amlunch.net>
2021-12-09 13:21:04 +0000

    Did some stuff with the thing.

--- roles/globalprotect/files/reinstall.sh
+++ roles/globalprotect/files/reinstall.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-dpkg -r globalprotect
-dpkg -i /opt/globalprotect/GlobalProtect_UI_deb-5.3.0.0-32.deb
+
+echo "Uninstalling Global Protect"
+sudo dpkg -r globalprotect
+
+echo "Installing Global Protect"
+sudo dpkg -i /opt/globalprotect/GlobalProtect_UI_deb-5.3.0.0-32.deb
+
+echo "Copying config file"
+cp /opt/globalprotect/pangpa.xml /home/wonko/.GlobalProtect/pangpa.xml
+
+echo "Reloading systemctl"
+systemctl --user daemon-reload
