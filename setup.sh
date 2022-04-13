be4b233482e0b4666b3d0e9080f3c212afe099e8
bhechinger <wonko@4amlunch.net>
2022-04-13 16:52:24 +0100

    apt-key causing me issues on 22.04, will figure that out later

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # run before doit
 sudo apt update
-sudo apt install -y ansible git
+sudo apt install -y ansible git python3-pip
 mkdir projects
 cd projects
 git clone https://github.com/bhechinger/deepthought.git
