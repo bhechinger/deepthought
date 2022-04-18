630a37b732afdbc0a0c138fb0a1e6e9c776abc30
bhechinger <wonko@4amlunch.net>
2022-04-18 20:12:20 +0100

    NodeSource doesn't yet support jammy.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 # run before doit
 sudo apt update
+sudo apt full-upgrade
 sudo apt install -y ansible git python3-pip
 mkdir projects
 cd projects
 git clone https://github.com/bhechinger/deepthought.git
 cd deepthought
+
+if ! test -f "${HOME}/projects/deepthought/secrets.yaml"; then
+  echo "Please place secrets.yaml into ~/projects/deepthought then press ENTER to continue"
+  read
+fi
+
 ./doit
